99cef4af8e5e9004d799a528da22450863bb5eb4
Alex Kondratiuk <alex.smesh@gmail.com>
2017-03-11 20:35:16 +0200

    up

--- CorebergWindowsFormsInstaller/CorebergWindowsFormsInstaller/Program.cs
+++ CorebergWindowsFormsInstaller/CorebergWindowsFormsInstaller/Program.cs
@@ -15,6 +15,7 @@ namespace CorebergWindowsFormsInstaller
             Application.SetCompatibleTextRenderingDefault(false);
             if (args.Length > 0)
             {
+                if (args[0] == "--tv-uninstall-9") TeamViewer.Uninstall(9);
                 if (args[0] == "--tv-uninstall-11") TeamViewer.Uninstall(11);
                 if (args[0] == "--tv-uninstall-12") TeamViewer.Uninstall(12);
                 if (args[0] == "--ocs-uninstall") OCS.Uninstall();
